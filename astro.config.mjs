58bd960b52a2fadd2b5c782fb262703c1bc6fc83
maushmipanda <86292955+maushmipanda@users.noreply.github.com>
2024-07-29 18:51:25 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    
+    site: 'https://maushmipanda.github.io',
+  base: 'move-git',
 });
